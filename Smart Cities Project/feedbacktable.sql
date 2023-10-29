79fe7d9732545bb38359c89ea6ee5f58f1837569
NicoleCarvalh <nicolecarvalho6@gmail.com>
2023-10-29 00:29:31 -0300

    Objetivo: organizando erros de display

---
+++ Smart Cities Project/feedbacktable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE 'feedback'(
+  `id` int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(225) NOT NULL,
+  `email` varchar(225) NOT NULL,
+  `feedback` varchar(1000) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO feedback(name,email,feedback) VALUES
+('Natalia', 'nataliaakaleshi@gmail.com', 'PHP ÉELEGAL'),
+('Jumbe.com', 'jumbe@gmail.com', 'AMO COMER PAO'),
+('Junin', 'junin@gmail.com', 'O Fuleco é o mascote da Copa'),
